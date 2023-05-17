0520ecfcde9d8953e71cd5d84c4773997985c225
Teresia <teresiamwangi26@gmail.com>
2023-05-17 09:06:24 +0300

    Complete Assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,77 @@
+ADMIN = "ADMIN"
+
 def admin_login(username, password)
-  # your code here
+  if username == ADMIN || username == "admin" && password == "12345"
+      "Access granted"
+  else 
+    "Access denied"
+  end
 end
+
+puts admin_login("sudo", "12345")
+puts admin_login("admin", "12345")
+puts admin_login("ADMIN", "12345")
+
+
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature <= 40 
+    "It's brisk out there!"
+  elsif 
+    temperature > 40 && temperature <= 65
+    "It's a little chilly out there!"
+  elsif
+    temperature >85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
+
+puts hows_the_weather(33)
+puts hows_the_weather(55)
+puts hows_the_weather(69)
+puts hows_the_weather(89)
+
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif 
+    num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
+puts fizzbuzz(3)
+puts fizzbuzz(4)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
+
+
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+    nil
+  end
 end
+
+puts calculator("+", 68, 1)
+puts calculator("-", 70, 1)
+puts calculator("*", 23, 3)
+puts calculator("/", 276, 4)
+puts calculator("nope", 138, 2)
 
